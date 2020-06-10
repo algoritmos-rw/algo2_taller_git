32d7628ee7db90e7edd84ac737551a7e9d0b2054
fedebrasburg <federico.brasburg@gmail.com>
2020-06-10 20:24:29 -0300

    Devolucion del PR

--- main.c
+++ main.c
@@ -1,9 +1,11 @@
+#include <stdio.h>
+
 int sumar(int a, int b) {
   return a + b;
 }
 
 int main() {
-  int resultado = sumar(1 , 4);
-  printf("El resultado fue %i", resultado);
+  int resultado = sumar(1, 4);
+  printf("El resultado fue %i \n", resultado);
   return 0;
 }
